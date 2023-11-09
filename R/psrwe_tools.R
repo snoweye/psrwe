d054cb03566e820a4050cdb973bcbdd4410d2bcd
snoweye <wccsnow@gmail.com>
2023-11-09 14:59:47 -0500

    typo

--- R/psrwe_tools.R
+++ R/psrwe_tools.R
@@ -738,7 +738,7 @@ get_ps_cl_km_none <- function(dta_psbor,
                  v_outcome     = v_outcome,
                  v_event       = v_event,
                  v_timet       = v_time,
-                 f_stratum     = f_stragum,
+                 f_stratum     = f_stratum,
                  f_overall_est = f_overall_est,
                  stderr_method = "none",
                  ...)
